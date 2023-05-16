d4e1a4d4cf27acac6ed413ff39c555cf9c670b1c
Yacub <bahanoev_yacub@mail.ru>
2023-05-16 20:06:33 +0300

    Начало

---
+++ soldier.js
@@ -0,0 +1,12 @@
+const soldier = {
+    name: 'Van Damm',
+    health: 10,
+    weapon: {
+        name: 'Kalashnicov',
+        cartridges: 120,
+    }
+    supplies: 0,
+  shoot(){},
+  reload() {},
+  hurt(){}, 
+};
