8c25a4ab79a4b6570419644d41efeb5823e48c38
Yacub <bahanoev_yacub@mail.ru>
2023-05-17 10:30:26 +0300

    задание выполнено

--- soldier.js
+++ soldier.js
@@ -1,12 +1,32 @@
 const soldier = {
-    name: 'Van Damm',
-    health: 10,
-    weapon: {
-        name: 'Kalashnicov',
-        cartridges: 120,
+  name: "Van Damm",
+  health: 10,
+  weapon: {
+    name: "Kalashnicov",
+    cartridges: 30,
+  },
+  supplies: 3,
+  fire() {
+    let result = "";
+    if (this.weapon.cartridges === 0) {
+      result = "обойма пуста. Перезаредитесь";
+      return result;
     }
-    supplies: 0,
-  shoot(){},
-  reload() {},
-  hurt(){}, 
+    this.weapon.cartridges--;
+    console.log("бах-бах");
+  },
+  recharge() {
+    if (this.supplies === 0) {
+      return "не осталось припасов";
+    }
+    this.weapon.cartridges = 30;
+    this.supplies--;
+    console.log("перезарядка...");
+  },
+  hurt() {
+    this.health--;
+    if (this.health === 0) {
+      console.log("Ты проиграл");
+    }
+  },
 };
